426bd7f05f6efbdb25b13c0018c7685125d6469a
Imran Ismail <imran.codely@gmail.com>
2015-11-27 16:25:22 +0800

    new mix app

---
+++ lib/sentry.ex
@@ -0,0 +1,3 @@
+defmodule Sentry do
+
+end
---
+++ test/sentry_test.exs
@@ -0,0 +1,7 @@
+defmodule SentryTest do
+  use ExUnit.Case
+  doctest Sentry
+
+  test "a user resource action should use equivalent policy" do
+  end
+end
