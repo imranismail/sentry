f0a1ddce1b9a95df492e65190708c7ad9245f2c9
Imran Ismail <imran.codely@gmail.com>
2015-11-27 16:26:47 +0800

    first test case

--- test/sentry_test.exs
+++ test/sentry_test.exs
@@ -1,7 +1,22 @@
+defmodule UserController do
+  use Sentry
+
+  def create(_conn, resource) do
+    authorize(resource)
+  end
+end
+
+defmodule UserPolicy do
+  def create(resource) do
+    {:ok, resource}
+  end
+end
+
 defmodule SentryTest do
   use ExUnit.Case
   doctest Sentry
 
   test "a user resource action should use equivalent policy" do
+    assert {:ok, resource} = UserController.create("connection", %{name: "Imran"})
   end
 end
