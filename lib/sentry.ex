af74389aae565e7662dd22150269af6c122e6172
Imran Ismail <imran.codely@gmail.com>
2015-11-29 04:25:36 +0800

    import Sentry.Authorizer

--- lib/sentry.ex
+++ lib/sentry.ex
@@ -1,7 +1,7 @@
 defmodule Sentry do
   defmacro __using__(_opts) do
     quote do
-      import Sentry.Authorizer, only: [authorize: 2, authorize: 3]
+      import Sentry.Authorizer
     end
   end
 end
