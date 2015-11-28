3bca85b91d45698020e28f7a5ddb941fcc2199d8
Imran Ismail <imran.codely@gmail.com>
2015-11-29 03:24:45 +0800

    test to be redone

--- test/sentry_test.exs
+++ test/sentry_test.exs
@@ -1,28 +1,8 @@
-defmodule MyApp.User do
-  defstruct name: nil, age: nil, email: nil
-end
-
-defmodule MyApp.UserController do
-  use Sentry
-
-  def create(conn, resource) do
-    authorize(conn, resource)
-  end
-end
-
-defmodule MyApp.UserPolicy do
-  def create(conn, resource) do
-    {:ok, resource}
-  end
-end
-
 defmodule SentryTest do
   use ExUnit.Case
   doctest Sentry
 
-  test "a resource controller action should use similarly named resource policy action" do
-    user = %MyApp.User{name: "John Doe", age: 23, email: "john.doe@example.com"}
-    conn = "this should be the %Plug.Conn{} struct"
-    assert {:ok, _resource} = MyApp.UserController.create(conn, user)
+  test "this should pass" do
+    assert 1 + 1 == 2
   end
 end
