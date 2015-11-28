218c1ff8ca4f318c3e265e8e1c57bcc3b750ae06
Imran Ismail <imran.codely@gmail.com>
2015-11-28 21:13:15 +0800

    changed from resource model to changeset and renamed authorize -> authorized?

--- lib/sentry.ex
+++ lib/sentry.ex
@@ -10,8 +10,8 @@ defmodule Sentry do
   @doc """
   Authorize a resource by running the similarly named function in the similarly named policy module
   """
-  def authorize(conn, resource, function) do
-    module_parts = resource.__struct__ |> Module.split
+  def authorized?(conn, changeset, function) do
+    module_parts = changeset.model.__struct__ |> Module.split
 
     policy_module =
       module_parts
@@ -23,16 +23,15 @@ defmodule Sentry do
       |> List.replace_at(length(module_parts) - 1, policy_module)
       |> Module.concat()
 
-    apply(module, function, [conn, resource])
+    apply(module, function, [conn, changeset])
   end
 
   @doc """
   Macro for authorizing resource which will use the lexically scoped module and function name
   """
-  defmacro authorize(conn, resource) do
+  defmacro authorized?(conn, changeset) do
     quote do
-      {function, _arity} = __ENV__.function
-      authorize(unquote(conn), unquote(resource), function)
+      authorized?(unquote(conn), unquote(changeset), elem(__ENV__.function, 0))
     end
   end
 end
