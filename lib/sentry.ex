2673ea94f1d9d731641a8a1228e54b4905b9937e
Imran Ismail <imran.codely@gmail.com>
2015-12-02 15:41:09 +0800

    WIP plug

--- lib/sentry.ex
+++ lib/sentry.ex
@@ -1,8 +1,18 @@
 defmodule Sentry do
+  alias Plug.Conn
+
   defmacro __using__(_opts) do
     quote do
       import Sentry.Authorizer
       import Sentry.Authenticator
     end
+  end
+
+  def init(options \\ []) do
+    options = Keyword.merge(Application.get_env(:sentry, Sentry), options)
+  end
+
+  def call(conn, options) do
+    Conn.put_private(conn, :sentry_options, options)
   end
 end
