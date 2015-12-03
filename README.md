b215abc2cfc7659226b4b980c896d08f562bb2ee
Imran Ismail <imran.codely@gmail.com>
2015-12-03 14:37:41 +0800

    version bump to 0.2.0

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Add sentry to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
-  [{:sentry, "~> 0.1"}]
+  [{:sentry, "~> 0.2"}]
 end
 ```
 
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sentry.Mixfile do
 
   def project do
     [app: :sentry,
-     version: "0.1.3",
+     version: "0.2.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
