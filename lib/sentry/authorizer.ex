9605da3883458e70234b676b6d55fa1fce656e7d
Imran Ismail <imran.codely@gmail.com>
2015-12-07 01:00:26 +0800

    better checks for phoenix and also added a new exception

--- lib/sentry/authorizer.ex
+++ lib/sentry/authorizer.ex
@@ -1,73 +1,22 @@
 defmodule Sentry.Authorizer do
   alias Ecto.Changeset
-  alias Sentry.Exception
+  alias Sentry.Helpers
 
   def authorize(conn, arg \\ nil, function \\ nil)
-
   def authorize(conn, module, [{function, args}]) do
-    apply_policy(module, function, [conn, args])
+    Helpers.apply_policy(module, function, [conn] ++ [args])
   end
-
-  def authorize(conn, %Changeset{} = arg, function) do
-    policy   = policy_module(arg.model.__struct__)
-    function = function || conn.private[:sentry_function]
-
-    apply_policy(policy, function, [conn, arg])
+  def authorize(conn, %Changeset{} = changeset, function) do
+    Helpers.verify_phoenix_controller!(conn)
+    policy   = Helpers.policy_module(changeset.model.__struct__)
+    function = function || conn.private[:phoenix_controller]
+    Helpers.apply_policy(policy, function, [conn, changeset])
   end
-
-  def authorize(conn, arg, _function) do
-    policy   = policy_module(conn.private[:sentry_module], "Controller")
-    function = conn.private[:sentry_function]
-
-    apply_policy(policy, function, [conn, arg])
-  end
-
-  defp apply_policy(module, function, args) do
-    check_policy!(module)
-
-    case apply(module, function, args) do
-      {:ok, result}    -> {:ok, result}
-      {:error, reason} -> {:error, reason}
-      _                -> raise Exception.MatchError, module: module,
-                                                      function: function,
-                                                      arity: length(args)
-    end
-  end
-
-  defp check_policy!(policy) do
-    if Code.ensure_loaded?(policy) do
-      policy
-    else
-      raise Exception.UndefinedPolicyError, policy: policy
-    end
-  end
-
-  defp policy_module(module, suffix \\ "") do
-    module_parts = Module.split(module)
-
-    policy = module_parts
-    |> List.last
-    |> unsuffix(suffix)
-    |> suffix("Policy")
-
-    module_parts
-    |> List.replace_at(length(module_parts) - 1, policy)
-    |> Module.concat
-  end
-
-  @spec suffix(String.t, String.t) :: String.t
-  defp suffix(alias, suffix) do
-    alias <> suffix
-  end
-
-  @spec unsuffix(String.t, String.t) :: String.t
-  def unsuffix(value, suffix) do
-    string = to_string(value)
-    suffix_size = byte_size(suffix)
-    prefix_size = byte_size(string) - suffix_size
-    case string do
-      <<prefix::binary-size(prefix_size), ^suffix::binary>> -> prefix
-      _ -> string
-    end
+  def authorize(conn, args, function) do
+    Helpers.verify_phoenix_controller!(conn)
+    policy   = Helpers.policy_module(conn.private[:phoenix_controller],
+                             "Controller")
+    function = function || conn.private[:phoenix_action]
+    Helpers.apply_policy(policy, function, [conn] ++ [args])
   end
 end
