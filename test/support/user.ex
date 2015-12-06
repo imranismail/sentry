6f6ebe3a9a9e540430be24d67f0b59a3309edce5
Imran Ismail <imran.codely@gmail.com>
2015-12-07 01:01:36 +0800

    add user

---
+++ test/support/user.ex
@@ -0,0 +1,27 @@
+defmodule Sentry.User do
+  use Ecto.Model
+
+  schema "users" do
+    field :email, :string
+    field :encrypted_password, :string
+    field :password, :string, virtual: true
+
+    timestamps
+  end
+
+  @required_fields ~w(email password)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+    |> unique_constraint(:email)
+    |> validate_length(:password, min: 1)
+  end
+end
