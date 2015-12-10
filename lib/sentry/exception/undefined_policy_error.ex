fac5930e1bc5e1e86cdbf8b67c41cdcb06a8f809
Imran Ismail <imran.codely@gmail.com>
2015-12-10 22:39:45 +0800

    cleanup exceptions

--- lib/sentry/exception/undefined_policy_error.ex
+++ lib/sentry/exception/undefined_policy_error.ex
@@ -1,9 +1,12 @@
 defmodule Sentry.Exception.UndefinedPolicyError do
-  defexception plug_status: 500, message: "undefined policy", policy: nil
+  defexception plug_status: 500, message: "undefined policy.", policy: nil
 
   def exception(opts) do
     policy = Keyword.fetch!(opts, :policy) |> Module.split |> Enum.join(".")
-
-    %Sentry.Exception.UndefinedPolicyError{message: "undefined policy: #{policy}", policy: policy}
+    %Sentry.Exception.UndefinedPolicyError
+    {
+      message: "undefined policy: #{policy}.",
+      policy: policy
+    }
   end
 end
