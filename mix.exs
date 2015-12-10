2d1c1adb10e1808ff3b9fe38b340fbd000568e33
Imran Ismail <imran.codely@gmail.com>
2015-12-10 22:42:54 +0800

    version bump to 0.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sentry.Mixfile do
   def project do
     [
       app: :sentry,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.1",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
