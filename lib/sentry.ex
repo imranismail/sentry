df3f0648d3b5a3592ca4b55eab88f9f74be6ef1b
Imran Ismail <imran.codely@gmail.com>
2015-12-30 11:11:06 +0800

    extracted to a dedicated plug

--- lib/sentry.ex
+++ lib/sentry.ex
@@ -1,41 +1,17 @@
 defmodule Sentry do
-  import Sentry.Helpers, only: [fetch_private!: 2, verify_phoenix_deps!: 0]
-  alias Sentry.Auth
+  import Sentry.Helpers
 
-  def init(opts) do
+  def model do
     verify_phoenix_deps!
-    opts
-  end
 
-  def call(conn, [policy: policy, action: action, args: args]) do
-    apply(policy, action, [conn] ++ args)
-  end
-
-  def call(conn, [policy: policy, action: action]) do
-    apply(policy, action, [conn])
-  end
-
-  def call(conn, [action: action, args: args]) do
-    Auth.authorize(conn, action, args)
-  end
-
-  def call(conn, [action: action]) do
-    Auth.authorize(conn, action)
-  end
-
-  def call(conn, [args: args]) do
-    Auth.authorize(conn, args)
-  end
-
-  def call(conn, _opts) do
-    Auth.authorize(conn)
-  end
-
-  defmacro __using__(_options) do
     quote do
-      unquote(verify_phoenix_deps!)
-      alias Sentry.Auth
-      alias Sentry.Tokenizer
+      def confirmed?(user) do
+        if user, do: !!user.confirmed_at
+      end
     end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
   end
 end
